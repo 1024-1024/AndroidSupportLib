f656e33f6f45663b84facb232eaec723fcb2f4dc
zhangweilong <weilongzhang16@gmail.com>
2016-06-08 11:19:14 +0800

    --

--- http/src/main/java/com/freedom/coder/http/custom/HttpSender.java
+++ http/src/main/java/com/freedom/coder/http/custom/HttpSender.java
@@ -5,18 +5,39 @@ import android.text.TextUtils;
 
 import com.freedom.coder.http.custom.bean.UrlCacheBean;
 
-import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by weilongzhang on 16/6/2.
  */
 public class HttpSender {
 
-    public static void sendPost(final UrlCacheBean urlCacheBean, final HashMap<String, String>
+    public static void sendPost(final UrlCacheBean urlCacheBean, final Map<String, String>
             paramMap, final boolean showLoading, final HttpCallbackListener listener) {
-        boolean needCache = urlCacheBean.isNeedCache();
-        final String url = urlCacheBean.getUrl();
+        HttpRequest request = new HttpRequest();
+        request.setUrlCacheBean(urlCacheBean);
+        request.setRequestMehtod(HttpUtils.RequestMehtod.POST);
+        request.setLoading(showLoading);
+        request.setListener(listener);
+        request.setParam(paramMap);
+        sendRequest(request);
+    }
 
+    public static void sendGet(UrlCacheBean urlCacheBean, Map<String, String> paramMap, final
+    boolean showLoading, final HttpCallbackListener listener) {
+        HttpRequest request = new HttpRequest();
+        request.setUrlCacheBean(urlCacheBean);
+        request.setRequestMehtod(HttpUtils.RequestMehtod.GET);
+        request.setLoading(showLoading);
+        request.setListener(listener);
+        request.setParam(paramMap);
+        sendRequest(request);
+    }
+
+
+    private static void sendRequest(final HttpRequest request) {
+        boolean needCache = request.getUrlCacheBean().isNeedCache();
+        final String url = request.getUrlCacheBean().getUrl();
         if (needCache) {
             //从缓存中取数据
             new AsyncTask<String, Void, Void>() {
@@ -24,7 +45,7 @@ public class HttpSender {
                 @Override
                 protected void onPreExecute() {
                     super.onPreExecute();
-                    if (showLoading) {
+                    if (request.isLoading()) {
 
                     }
                 }
@@ -33,23 +54,20 @@ public class HttpSender {
                 protected Void doInBackground(String... params) {
                     UrlCacheBean cacheBean = HttpCacheUtils.getInstance().getCacheBean(params[0]);
                     if (TextUtils.isEmpty(cacheBean.getData())) {
-                        HttpUtils.sendHttpRequest(HttpUtils.RequestMehtod.POST, params[0],
-                                paramMap, listener);
+                        HttpUtils.sendHttpRequest(request.getRequestMehtod(), params[0], request
+                                .getParam(), request.getListener());
                     } else {
-                        if (listener != null) {
-                            listener.onCache(cacheBean.getData());
+                        if (request.getListener() != null) {
+                            request.getListener().onCache(cacheBean.getData());
                         }
                     }
                     return null;
                 }
             }.execute(url);
         } else {
-            HttpUtils.sendHttpRequest(HttpUtils.RequestMehtod.POST, url, paramMap, listener);
+            HttpUtils.sendHttpRequest(HttpUtils.RequestMehtod.POST, url, request.getParam(),
+                    request.getListener());
         }
-    }
-
-    public static void sendGet(UrlCacheBean urlCacheBean, HashMap<String, String> paramMap) {
-
     }
 
 }
--- http/src/main/java/com/freedom/coder/http/custom/HttpUtils.java
+++ http/src/main/java/com/freedom/coder/http/custom/HttpUtils.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,7 +28,7 @@ public class HttpUtils {
         GET,
     }
 
-    public static void sendHttpRequest(RequestMehtod method, String url, HashMap<String, String>
+    public static void sendHttpRequest(RequestMehtod method, String url, Map<String, String>
             param, HttpCallbackListener listener) {
 
         HttpClient client = new DefaultHttpClient();
