59684818830f796d47c09d71b154d44f638313e7
维龙张 <weilongzhang@weilongzhangdeMacBook-Pro.local>
2016-06-29 14:38:19 +0800

    add luncher

---
+++ http/src/main/java/MainActivity.java
@@ -0,0 +1,12 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by weilongzhang on 16/6/29.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
