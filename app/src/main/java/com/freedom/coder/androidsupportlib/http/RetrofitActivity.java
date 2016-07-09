9a07af1d166eb8073365096dd8f9286827cf07eb
维龙张 <weilongzhang@weilongzhangdeMacBook-Pro.local>
2016-07-10 00:01:30 +0800

    retrofit

--- app/src/main/java/com/freedom/coder/androidsupportlib/http/RetrofitActivity.java
+++ app/src/main/java/com/freedom/coder/androidsupportlib/http/RetrofitActivity.java
@@ -2,6 +2,8 @@ package com.freedom.coder.androidsupportlib.http;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.freedom.coder.androidsupportlib.R;
 
@@ -10,9 +12,36 @@ import com.freedom.coder.androidsupportlib.R;
  */
 public class RetrofitActivity extends Activity {
 
+    private TextView mData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit);
+
+        mData = (TextView) findViewById(R.id.tv_data);
+
+        findViewById(R.id.btn_get).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrofitGet();
+            }
+        });
+
+        findViewById(R.id.btn_post).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrofitPost();
+            }
+        });
+
     }
+
+    private void retrofitGet() {
+
+    }
+    private void retrofitPost() {
+
+    }
+
 }
