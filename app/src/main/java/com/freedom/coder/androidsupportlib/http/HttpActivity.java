c6b2c048c38590c8deba56e05ea1a25a7f814835
维龙张 <weilongzhang@weilongzhangdeMacBook-Pro.local>
2016-07-15 15:52:21 +0800

    整理

--- app/src/main/java/com/freedom/coder/androidsupportlib/http/HttpActivity.java
+++ app/src/main/java/com/freedom/coder/androidsupportlib/http/HttpActivity.java
@@ -1,18 +1,14 @@
 package com.freedom.coder.androidsupportlib.http;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.freedom.coder.androidsupportlib.R;
-import com.freedom.coder.androidsupportlib.http.OkHttpActivity;
-import com.freedom.coder.androidsupportlib.http.RetrofitActivity;
-import com.freedom.coder.androidsupportlib.http.VolleyActivity;
-import com.freedom.coder.androidsupportlib.http.XUtilsActivity;
 
-public class HttpActivity extends AppCompatActivity implements View.OnClickListener{
+public class HttpActivity extends Activity implements View.OnClickListener{
 
     private Button mVolley,mOkHttp,mXutils,mRetrofit;
     private Intent intent;
@@ -20,7 +16,7 @@ public class HttpActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_http);
         mVolley = (Button) findViewById(R.id.btn_volley);
         mOkHttp = (Button) findViewById(R.id.btn_okHttp3);
         mXutils = (Button) findViewById(R.id.btn_xUtils);
--- app/src/main/java/com/freedom/coder/androidsupportlib/rxjava/RxJavaActivity.java
+++ app/src/main/java/com/freedom/coder/androidsupportlib/rxjava/RxJavaActivity.java
@@ -1,20 +1,26 @@
 package com.freedom.coder.androidsupportlib.rxjava;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.freedom.coder.androidsupportlib.R;
 
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by weilongzhang on 16/7/15.
  *
  * @link https://gank.io/post/560e15be2dca930e00da1083
- *
  */
 public class RxJavaActivity extends Activity {
 
@@ -35,23 +41,50 @@ public class RxJavaActivity extends Activity {
 
         observable.subscribe(observer);
 
+        final int drawableRes = R.mipmap.ic_launcher;
+        final ImageView imageView = (ImageView) findViewById(R.id.iv);
+        Observable.create(new Observable.OnSubscribe<Drawable>() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void call(Subscriber<? super Drawable> subscriber) {
+                Drawable drawable = getTheme().getDrawable(drawableRes);
+                subscriber.onNext(drawable);
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
+                .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程
+                .subscribe(new Observer<Drawable>() {
+                    @Override
+                    public void onNext(Drawable drawable) {
+                        imageView.setImageDrawable(drawable);
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(RxJavaActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 
     private Observer<String> observer = new Observer<String>() {
         @Override
         public void onCompleted() {
-            Log.d("zwl","onCompleted");
+            Log.d("zwl", "onCompleted");
         }
 
         @Override
         public void onError(Throwable e) {
-            Log.d("zwl","onError");
+            Log.d("zwl", "onError");
         }
 
         @Override
         public void onNext(String s) {
-            Log.d("zwl","onNext:" + s);
+            Log.d("zwl", "onNext:" + s);
         }
     };
 
