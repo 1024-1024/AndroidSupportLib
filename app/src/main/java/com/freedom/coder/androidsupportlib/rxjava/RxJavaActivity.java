76d1ebb2aacbcd5d85272f69805a3cd6f8a8f7ec
维龙张 <weilongzhang@weilongzhangdeMacBook-Pro.local>
2016-07-15 13:33:20 +0800

    rxjava

---
+++ app/src/main/java/com/freedom/coder/androidsupportlib/rxjava/RxJavaActivity.java
@@ -0,0 +1,15 @@
+package com.freedom.coder.androidsupportlib.rxjava;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by weilongzhang on 16/7/15.
+ */
+public class RxJavaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
