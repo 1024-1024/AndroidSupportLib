89c12de071393e928d40dfaac50288539ef1d092
维龙张 <weilongzhang@weilongzhangdeMacBook-Pro.local>
2016-07-15 15:15:48 +0800

    aa

--- app/src/main/java/com/freedom/coder/androidsupportlib/MainActivity.java
+++ app/src/main/java/com/freedom/coder/androidsupportlib/MainActivity.java
@@ -1,53 +1,32 @@
 package com.freedom.coder.androidsupportlib;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-import com.freedom.coder.androidsupportlib.http.OkHttpActivity;
-import com.freedom.coder.androidsupportlib.http.RetrofitActivity;
-import com.freedom.coder.androidsupportlib.http.VolleyActivity;
-import com.freedom.coder.androidsupportlib.http.XUtilsActivity;
+import com.freedom.coder.androidsupportlib.http.HttpActivity;
+import com.freedom.coder.androidsupportlib.rxjava.RxJavaActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private Button mVolley,mOkHttp,mXutils,mRetrofit;
-    private Intent intent;
+/**
+ * Created by weilongzhang on 16/7/15.
+ */
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mVolley = (Button) findViewById(R.id.btn_volley);
-        mOkHttp = (Button) findViewById(R.id.btn_okHttp3);
-        mXutils = (Button) findViewById(R.id.btn_xUtils);
-        mRetrofit = (Button) findViewById(R.id.btn_retrofit2);
-        mVolley.setOnClickListener(this);
-        mRetrofit.setOnClickListener(this);
-        mXutils.setOnClickListener(this);
-        mOkHttp.setOnClickListener(this);
-        intent = new Intent();
+        setContentView(R.layout.main);
     }
 
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        switch (id) {
-            case R.id.btn_okHttp3:
-                intent.setClass(this, OkHttpActivity.class);
-                break;
-            case R.id.btn_retrofit2:
-                intent.setClass(this, RetrofitActivity.class);
-                break;
-            case R.id.btn_volley:
-                intent.setClass(this, VolleyActivity.class);
-                break;
-            case R.id.btn_xUtils:
-                intent.setClass(this, XUtilsActivity.class);
-                break;
-        }
+    public void http(View view) {
+        Intent intent = new Intent(this, HttpActivity.class);
         startActivity(intent);
     }
+
+    public void rxJava(View view) {
+        Intent intent = new Intent(this, RxJavaActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/freedom/coder/androidsupportlib/rxjava/RxJavaActivity.java
+++ app/src/main/java/com/freedom/coder/androidsupportlib/rxjava/RxJavaActivity.java
@@ -2,14 +2,74 @@ package com.freedom.coder.androidsupportlib.rxjava;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.freedom.coder.androidsupportlib.R;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
 
 /**
  * Created by weilongzhang on 16/7/15.
+ *
+ * @link https://gank.io/post/560e15be2dca930e00da1083
+ *
  */
 public class RxJavaActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxjava);
+
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("Hi");
+                subscriber.onNext("Aloha");
+                subscriber.onCompleted();
+            }
+        });
+
+        observable.subscribe(observer);
+
     }
+
+
+    private Observer<String> observer = new Observer<String>() {
+        @Override
+        public void onCompleted() {
+            Log.d("zwl","onCompleted");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.d("zwl","onError");
+        }
+
+        @Override
+        public void onNext(String s) {
+            Log.d("zwl","onNext:" + s);
+        }
+    };
+
+
+    private Subscriber<String> subscriber = new Subscriber<String>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+
+        }
+    };
 }
